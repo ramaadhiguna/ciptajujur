d6e5bb32be851ad5f8db236bb1fc0e6a43bdc17f
tajuk <35762060+tajuk@users.noreply.github.com>
2018-01-25 16:56:25 +0700

    lol

--- tambahpenjualan.php
+++ tambahpenjualan.php
@@ -8,8 +8,7 @@
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
   <!-- Bootstrap 3.3.7 -->
   <link rel="stylesheet" href="bower_components/bootstrap/dist/css/bootstrap.min.css">
-  <!-- Bootstrap select -->
-  <link rel="stylesheet" href="dist/css/bootstrap-select.css">  
+  <!-- Bootstrap select --> 
   <!-- Font Awesome -->
   <link rel="stylesheet" href="bower_components/font-awesome/css/font-awesome.min.css">
   <!-- Ionicons -->
@@ -302,7 +301,6 @@
 <!-- Bootstrap 3.3.7 -->
 <script src="bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
 <!-- Bootstrap select -->
-<script src="dist/js/bootstrap-select.js"></script>
 <!-- DataTables -->
 <script src="bower_components/datatables.net/js/jquery.dataTables.min.js"></script>
 <script src="bower_components/datatables.net-bs/js/dataTables.bootstrap.min.js"></script>
